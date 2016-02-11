ce83902082606eb0fe6bae45fbf15c4f3bf595e9
George Yarish <georgii_iarysh@epam.com>
2016-02-12 01:35:30 +0300

    init spring boot

---
+++ src/main/java/ru/spb/yarish/dm/DepositManagementAppApplication.java
@@ -0,0 +1,12 @@
+package ru.spb.yarish.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositManagementAppApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/spb/yarish/dm/DepositManagementAppApplicationTests.java
@@ -0,0 +1,18 @@
+package ru.spb.yarish.dm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DepositManagementAppApplication.class)
+@WebAppConfiguration
+public class DepositManagementAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
